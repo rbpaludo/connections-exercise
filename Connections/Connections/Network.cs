89cb7d7b13c98a5cab7a0e910146346b6d254eee
Rafael Beffart Paludo <rbpaludo@gmail.com>
2019-02-20 20:07:44 -0300

    Finalizacao do exercício de lógica

---
+++ Connections/Connections/Network.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Connections
+{
+    public class Network
+    {
+        private List<int>[] elementsTable;
+        
+        /// <summary>
+        /// Starts the network with the given number of elements.
+        /// </summary>
+        /// <param name="numElements">Size of the table (number of elements contained)</param>
+        /// <exception cref="Exception">Thrown when the given number of elements is invalid (lower than 1).</exception>
+        public Network(int numElements)
+        {
+            if (numElements < 1)
+            {
+                throw new Exception("The given number of elements is invalid (lower than 1)");
+            }
+            elementsTable = new List<int>[numElements];
+
+            for (var i = 0; i < elementsTable.Length; i++)
+            {
+                elementsTable[i] = new List<int>();
+            }
+        }
+
+        /// <summary>
+        /// Makes a new connection between two elements in the table.
+        /// </summary>
+        /// <param name="elem1">First element of the connection</param>
+        /// <param name="elem2">Second element of the connection</param>
+        /// <exception cref="Exception">Thrown in two occasions: the desired connection already exists or one of the 
+        ///     given numbers is invalid (lower than 1 or higher than the number of existing elements).</exception>
+        public void Connect(int elem1, int elem2)
+        {
+            
+            if (elem1 < 1 || elem1 > elementsTable.Length + 1 ||
+                elem2 < 1 || elem2 > elementsTable.Length + 1)
+            {
+                throw new Exception("One of the given elements is invalid (lower than 1 or higher than the number of " +
+                                    "existing elements).");
+            }
+            
+            if (elementsTable[elem1 - 1].Contains(elem2))
+            {
+                throw new Exception("This connection already exists.");
+            }
+            
+            elementsTable[elem1 - 1].Add(elem2);
+            elementsTable[elem2 - 1].Add(elem1);
+        }
+        
+        /// <summary>
+        /// Looks for a connection between the two given elements.
+        /// </summary>
+        /// <param name="elem1">First element of the connection</param>
+        /// <param name="elem2">Second element of the connection</param>
+        /// <returns>True if the connection between the two given elements exists in the network. 
+        ///     False if it doesn't.</returns>
+        /// <exception cref="Exception">Thrown if one of the given numbers is invalid (lower than 1 or higher than the 
+        ///     number of existing elements).</exception>
+        public bool Query(int elem1, int elem2)
+        {
+            
+            if (elem1 < 1 || elem1 > elementsTable.Length + 1 ||
+                elem2 < 1 || elem2 > elementsTable.Length + 1)
+            {
+                throw new Exception("Um dos elementos é inválido (menor que 1 ou maior que o número de elementos " +
+                                    "existentes)");
+            }
+            
+            // no connections in the required element
+            if (elementsTable[elem1 - 1].Count == 0)
+            {
+                return false;
+            }
+
+            // the connection is found
+            if (elementsTable[elem1 - 1].Contains(elem2))
+            {
+                return true;
+            }
+
+            // searches recursively for the desired connection through other connections. 
+            //TODO Search in a way other than recursively, because it can become really slow in big networks
+            foreach (var connections in elementsTable)
+            {
+                foreach (var connection in connections)
+                {
+                    if (Query(connection, elem2))
+                    {
+                        return true;
+                    }   
+                }
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ Connections/Connections/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.AccessControl;
+
+namespace Connections
+{
+    internal class Program
+    {
+        private static Network network;
+        
+        public static void Main(string[] args)
+        {
+            var option = "0";
+
+            Console.WriteLine("Hello, welcome to the connections project!\n" +
+                              "In this project we give you a network with a number of elements of your choice, make " +
+                              "connections between them and let you check the existence (or not) of connections.\n" +
+                              "\n");
+            while (parseInt(option) != 4)
+            {
+                Console.WriteLine("Please choose one of the following options:\n" +
+                                  "1 - Create a new network;\n" +
+                                  "2 - Make a new connection;\n" +
+                                  "3 - Check for the existence of a connection;\n" +
+                                  "4 - Exit.");
+
+                option = Console.ReadLine();
+
+                try
+                {
+                    var elem = 0;
+                    switch (parseInt(option))
+                    {
+                        case 1:
+                            Console.WriteLine("Inform the number of desired elements for the network: ");
+                            network = new Network(parseInt(Console.ReadLine()));
+                            break;
+                        case 2:
+                            Console.WriteLine("Inform the first element of the connection: ");
+                            elem = parseInt(Console.ReadLine());
+                            Console.WriteLine("Inform the second element of the connection: ");
+                            network.Connect(elem, parseInt(Console.ReadLine()));
+                            Console.WriteLine("The connection was successfully created!");
+                            break;
+                        case 3:
+                            Console.WriteLine("Inform the first element of the connection: ");
+                            elem = parseInt(Console.ReadLine());
+                            Console.WriteLine("Inform the second element of the connection: ");
+                            if (network.Query(elem, parseInt(Console.ReadLine())))
+                            {
+                                Console.WriteLine("The connection exists!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("The connection doesn't exist!");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Goodbye");
+                            break;
+                        default:
+                            Console.WriteLine("Your choice of operation is not in the list");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    option = "0";
+                }
+            }
+        }
+        
+        private static int parseInt(string value)
+        {
+            var valueInt = 0;
+            
+            if (!int.TryParse(value, out valueInt)) throw new Exception("The informed value is not a number.");
+            
+            return valueInt;
+        }
+    }
+}
